b702ebf0c472f52c770337e05abb1df3e93c6b10
ridwan1997 <ridwanmalik88@gmail.com>
2019-07-15 19:56:13 +0700

    Update with miniserver

--- app/src/main/java/com/malik/location/MainActivity.kt
+++ app/src/main/java/com/malik/location/MainActivity.kt
@@ -6,12 +6,28 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
+import androidx.navigation.fragment.NavHostFragment
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.*
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
+
+    val userName: String = "malik"
+    val pass: String = "malik"
+
+    private lateinit var requestQueue: RequestQueue
+
+    private lateinit var stringRequest: StringRequest
+    private lateinit var stringRequest2: JsonObjectRequest
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 
     fun loginOnPress(view: View) {
@@ -19,7 +35,51 @@ class MainActivity : AppCompatActivity() {
         val password = findViewById<EditText>(R.id.editTextPassword)
         val intent = Intent(this, NewHomeScreen::class.java)
         Log.d("Main Activity", "${username.text}, ${password.text}")
-        startActivity(intent)
+
+        val url = "http://192.168.0.104:5000"
+
+        val cache = DiskBasedCache(cacheDir, 1024 * 1024)
+
+        val network = BasicNetwork(HurlStack())
+
+        requestQueue = RequestQueue(cache, network).apply {
+            start()
+        }
+
+        stringRequest = StringRequest(Request.Method.GET, url,
+            Response.Listener { response ->
+                Toast.makeText(this, "Sukses", Toast.LENGTH_SHORT).show()
+                Log.d("RESPONSE", response.toString())
+            },
+            Response.ErrorListener { error -> Toast.makeText(this, "Error $error", Toast.LENGTH_SHORT).show() }
+            )
+
+        val jsonObject = JSONObject()
+        jsonObject.put("username", username.text)
+        jsonObject.put("password", password.text)
+
+
+        stringRequest2 = JsonObjectRequest(Request.Method.POST, "$url/auth", JSONObject(jsonObject.toString()),
+            Response.Listener { response ->
+                Log.d("RESPONSE_POST", response.toString())
+                startActivity(intent)
+                finish()
+        }, Response.ErrorListener { error ->
+                Log.d("RESPONSE_ERROR", error.toString())
+            })
+
+        stringRequest.tag = "GET_REQUEST"
+        stringRequest2.tag= "POST_REQUEST"
+
+
+        requestQueue.add(stringRequest)
+        requestQueue.add(stringRequest2)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        requestQueue.cancelAll("GET_REQUEST")
+        requestQueue.cancelAll("POST_REQUEST")
     }
 
 }
--- app/src/main/java/com/malik/location/TimelineActivity.kt
+++ app/src/main/java/com/malik/location/TimelineActivity.kt
@@ -14,37 +14,68 @@ import android.widget.ListView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.DialogFragment
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.*
 import org.json.JSONObject
 import java.io.InputStream
 import java.lang.Exception
 
 class TimelineActivity : AppCompatActivity() {
 
+    private lateinit var requestQueue: RequestQueue
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_timeline)
-        var obj = JSONObject(loadFromAssets())
-        val data = obj.getJSONArray("data")
-        Log.d("HOME", "$data")
-        var listData = ArrayList<User>()
-        var adapter: MyUserAdapter? = null
 
-        if (data != null) {
-            for (i in 0 until data.length()) {
-                var e = User()
-                e.name = data.getJSONObject(i).getString("name")
-                e.phone = data.getJSONObject(i).getString("phone")
-                listData.add(e)
-            }
+        val cache = DiskBasedCache(cacheDir, 1024 * 1024)
+
+        val network = BasicNetwork(HurlStack())
+
+        requestQueue = RequestQueue(cache, network).apply {
+            start()
         }
 
-        adapter = MyUserAdapter(this, listData)
+        val url = "http://192.168.0.104:5000"
 
-        val lv = findViewById<ListView>(R.id.listview1)
-        lv.adapter = adapter
-        lv.setOnItemClickListener { adapterView, view, i, l ->
-            Toast.makeText(this, listData[i].name, Toast.LENGTH_LONG).show()
-        }
+        val stringRequest = JsonObjectRequest(Request.Method.GET, "$url/get_patrol", null,
+            Response.Listener { response ->
+                Toast.makeText(this, "Sukses", Toast.LENGTH_SHORT).show()
+                Log.d("RESPONSE", response.toString())
+
+                val data = response.getJSONArray("data")
+                Log.d("TAG_Timeline", data.toString())
+                Log.d("HOME", "$data")
+                var listData = ArrayList<User>()
+                var adapter: MyUserAdapter? = null
+
+                if (data != null) {
+                    for (i in 0 until data.length()) {
+                        var e = User()
+                        e.name = data.getJSONObject(i).getString("name")
+                        e.phone = data.getJSONObject(i).getString("phone")
+                        listData.add(e)
+                    }
+                }
+
+                adapter = MyUserAdapter(this, listData)
+
+                val lv = findViewById<ListView>(R.id.listview1)
+                lv.adapter = adapter
+                lv.setOnItemClickListener { adapterView, view, i, l ->
+                    Toast.makeText(this, listData[i].name, Toast.LENGTH_LONG).show()
+                }
+            },
+            Response.ErrorListener { error -> Toast.makeText(this, "Error $error", Toast.LENGTH_SHORT).show() }
+        )
+
+
+        requestQueue.add(stringRequest)
+
+//        var obj = JSONObject(loadFromAssets())
+
     }
 
 
